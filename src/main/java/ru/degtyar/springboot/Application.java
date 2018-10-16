1b1178fa8c02d39de5ea16481b70baf18e2cdd81
zeikks <zeik@ngs.ru>
2018-10-16 17:39:30 +0700

    Задание  №5

--- src/main/java/ru/degtyar/springboot/Application.java
+++ src/main/java/ru/degtyar/springboot/Application.java
@@ -2,13 +2,17 @@ package ru.degtyar.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+//@EnableZuulProxy
 @SpringBootApplication
-@EnableAspectJAutoProxy
+@EnableEurekaServer
+//@EnableAspectJAutoProxy
 public class Application {
 
 	public static void main(String[] args) {
